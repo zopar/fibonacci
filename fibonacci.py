1383933b94cbba2f5b46b126c0b4e15ee02f42a7
armando.trinchillo <armando.trinchillo@ericsson.com>
2020-09-04 03:30:37 +0200

    Added more test cases and fixed bug on fibonacci for 0

--- fibonacci.py
+++ fibonacci.py
@@ -4,6 +4,17 @@
 Returns:
     [type]: [description]
 """
+
+import os
+import sys
+import datetime
+import logging
+
+# Enable logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s %(name)s %(levelname)s %(message)s')
+# create logger
+logger = logging.getLogger('fibonacci:')
+
 # We use a generator
 def fibonacci(number):
     """[summary]
@@ -17,41 +28,80 @@ def fibonacci(number):
     
     # a is a small cache, we sum based on it
     a=[0,1]
+    # In case of 0  len of range is 0 and do not do anything
+    if number == 0:
+        yield number
     for i in range(0, number):
         if i == 0:
             yield i
-        elif i == 1:
+        if i == 1:
             yield i
         else:
             a[0] = a[0]+a[1]
             yield a[0]
             # We need inversion because we sum always on first element
             a[0], a[1] = a[1], a[0]
-        
 
-def to_file(result):
-    pass
+def check_and_rename(file_out):
+    if os.path.isfile(file_out): 
+        try:
+            os.rename(file_out, file_out+"_"+datetime.datetime.now().strftime("%d-%m-%Y-%H-%M-%S"))
+        except Exception as e:
+            logger.error(e + file_out)
+        logger.info("Renaming already existent file "+file_out)
+                
+def to_file(file_out, g):
+    """[summary]
+
+    Args:
+        number ([type]): [description]
+
+    Returns:
+        [type]: [description]
+    """
+    check_and_rename(file_out)
+        
+    try:
+        f=open(file_out, 'a')
+        # looping over generator, this not increase time complexity  
+        for result in g:
+            f.write(str(result) + '\n')
+        f.close()
+    except Exception as e:
+        logger.error(e + file_out)
+        sys.exit(1)
 
 def main():
     """[summary]
     """
-    print("This software prints fibonacci numbers in a file")
+    workdir=os.getcwd()+"/"
+    basename="fibonacci"
+    
+    logger.info("This software prints fibonacci numbers in a file")
 
     # Manage exception value error if a non numeric value is passed
     try:
-        number = int(input("Enter a positive number: "))
+        number = int(input("Please, enter a positive number: "))
     except ValueError:
-        print("ERROR: Non-numeric values are not allowed")
-        exit(1)
+        logger.error("Non-numeric values are not allowed")
+        sys.exit(1)
 
     # Be sure that the passed number is not negative
     if number < 0:
-        print("ERROR: Negative values are not allowed")
-        exit(1)
+        logger.error("Negative values are not allowed")
+        sys.exit(1)
         
-    # looping over generator, this not increase time complexity     
-    for k in fibonacci(number):
-        print(k)
+    # file to write
+    file_out=workdir+basename+"_"+str(number)
+    
+    logger.info("Calculating, it may take time, saving result on "+file_out)
+    
+    # call generator 
+    g = fibonacci(number)
+    # write generator to file
+    to_file(file_out, g)
+    
+    logger.info("Done")
 
 if __name__ == "__main__":
     main()
--- tests/test_fibonacci.py
+++ tests/test_fibonacci.py
@@ -2,7 +2,9 @@
 """[summary]
 """
 import pytest
+import io
 import fibonacci
+import filecmp
 
 def test_check_non_numeric(mocker):
     """[summary]
@@ -18,6 +20,24 @@ def test_check_positive(mocker):
     with pytest.raises(SystemExit):
         fibonacci.main()
 
+def test_fibonacci_generator_0_to_0():
+    """[summary]
+    We use next to be sure the function is a generator
+    """
+    generated = fibonacci.fibonacci(0)
+    expected = [0]
+    for i in expected:
+        assert next(generated) == i
+        
+def test_fibonacci_generator_0_to_1():
+    """[summary]
+    We use next to be sure the function is a generator
+    """
+    generated = fibonacci.fibonacci(1)
+    expected = [0, 1]
+    for i in expected:
+        assert next(generated) == i
+
 def test_fibonacci_generator_0_to_6():
     """[summary]
     We use next to be sure the function is a generator
@@ -26,3 +46,16 @@ def test_fibonacci_generator_0_to_6():
     expected = [0, 1, 1, 2, 3, 5]
     for i in expected:
         assert next(generated) == i
+
+def test_to_file(tmpdir):
+    expected_file = tmpdir.join('expected')
+    with open(expected_file, 'a') as out:
+       out.write('0' + '\n')
+       out.write('1' + '\n')
+    fib_file = tmpdir.join('fibonacci_1')
+    g = fibonacci.fibonacci(1)
+    fibonacci.to_file(fib_file, g) 
+    assert filecmp.cmp(expected_file, fib_file, shallow=False)
+
+def test_check_and_rename():
+    pass
